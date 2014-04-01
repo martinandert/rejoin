7f80ad1bee028d1a759d5342ceb5a932fb03ba52
martinandert <mandert@gmail.com>
2014-04-01 10:50:12 +0200

    36th commit

---
+++ test/validations/ExclusionValidatorTest.js
@@ -0,0 +1,146 @@
+'use strict';
+
+var assert  = require('assert');
+var async   = require('async');
+var Topic   = require('../models/Topic');
+
+suite('exclusion validator', function() {
+  teardown(function() {
+    Topic.clearValidations();
+  });
+
+  test('default validatesExclusionOf', function(done) {
+    Topic.validatesExclusionOf('title', { in: ['abe', 'monkey'] });
+
+    async.parallel([
+      tc({ title: 'something', content: 'abc' }, true),
+      tc({ title: 'monkey',    content: 'abc' }, false)
+    ], done);
+
+    function tc(attrs, result, message) {
+      return function(cb) {
+        Topic.new(attrs, function(err, topic) {
+          if (err) { cb(err); return; }
+
+          topic.validate(function(err, valid) {
+            if (err) { cb(err); return; }
+
+            assert.strictEqual(result, valid);
+
+            cb();
+          });
+        });
+      };
+    }
+  });
+
+  test('validatesExclusionOf with allowNull', function(done) {
+    Topic.validatesExclusionOf('title', { in: 'a b c d e f g'.split(' '), allowNull: true });
+
+    async.parallel([
+      tc({ title: 'a',  content: 'abc' }, false),
+      tc({ title: '',   content: 'abc' }, true),
+      tc({ title: null, content: 'abc' }, true)
+    ], done);
+
+    function tc(attrs, result, message) {
+      return function(cb) {
+        Topic.new(attrs, function(err, topic) {
+          if (err) { cb(err); return; }
+
+          topic.validate(function(err, valid) {
+            if (err) { cb(err); return; }
+
+            assert.strictEqual(result, valid);
+
+            cb();
+          });
+        });
+      };
+    }
+  });
+
+  test('validatesExclusionOf with custom message', function(done) {
+    Topic.validatesExclusionOf('title', { in: ['abe', 'monkey'], message: 'option %(value)s is restricted' });
+
+    Topic.new({ title: 'something', content: 'abc' }, function(err, topic) {
+      if (err) { done(err); return; }
+
+      topic.validate(function(err, valid) {
+        if (err) { done(err); return; }
+
+        assert(valid);
+
+        topic.setTitle('monkey');
+
+        topic.validate(function(err, valid) {
+          if (err) { done(err); return; }
+
+          assert(!valid);
+          assert.deepEqual(['option monkey is restricted'], topic.getErrors().get('title'));
+
+          done();
+        });
+      });
+    });
+  });
+
+  test('validatesExclusionOf with within option', function(done) {
+    Topic.validatesExclusionOf('title', { within: ['abe', 'monkey'] });
+
+    Topic.new({ title: 'something', content: 'abc' }, function(err, topic) {
+      if (err) { done(err); return; }
+
+      topic.validate(function(err, valid) {
+        if (err) { done(err); return; }
+
+        assert(valid);
+
+        topic.setTitle('monkey');
+
+        topic.validate(function(err, valid) {
+          if (err) { done(err); return; }
+
+          assert(!valid);
+          assert(topic.getErrors().get('title').length > 0);
+
+          done();
+        });
+      });
+    });
+  });
+
+  test('validatesExclusionOf with function', function(done) {
+    Topic.validatesExclusionOf('title', { within: function(topic) { return topic.getAuthorName() === 'sikachu' ? ['monkey', 'elephant'] : ['abe', 'wasabi']; } });
+
+    Topic.new(function(err, topic) {
+      if (err) { done(err); return; }
+
+      topic.setAuthorName('sikachu');
+      topic.setTitle('elephant');
+
+      topic.validate(function(err, valid) {
+        if (err) { done(err); return; }
+
+        assert(!valid);
+
+        topic.setTitle('wasabi');
+
+        topic.validate(function(err, valid) {
+          if (err) { done(err); return; }
+
+          assert(valid);
+
+          done();
+        });
+      });
+    });
+  });
+
+  test('validatesExclusionOf with invalid args', function() {
+    assert.throws(function() { Topic.validatesExclusionOf('title');                 });
+    assert.throws(function() { Topic.validatesExclusionOf('title', { in: null });   });
+    assert.throws(function() { Topic.validatesExclusionOf('title', { in: 0 });      });
+    assert.throws(function() { Topic.validatesExclusionOf('title', { in: 'abc' });  });
+  });
+});
---
+++ test/validations/InclusionValidatorTest.js
@@ -0,0 +1,170 @@
+'use strict';
+
+var assert  = require('assert');
+var async   = require('async');
+var Topic   = require('../models/Topic');
+
+suite('inclusion validator', function() {
+  teardown(function() {
+    Topic.clearValidations();
+  });
+
+  test('default validatesInclusionOf', function(done) {
+    Topic.validatesInclusionOf('title', { in: 'a b c d e f g'.split(' ') });
+
+    async.parallel([
+      tc({ title: 'a!',   content: 'abc' }, false),
+      tc({ title: 'a b',  content: 'abc' }, false),
+      tc({ title: null,   content: 'def' }, false)
+    ], function(err) {
+      if (err) { done(err); return; }
+
+      Topic.new({ title: 'a', content: 'I know you are but what am I?' }, function(err, topic) {
+        if (err) { done(err); return; }
+
+        topic.validate(function(err, valid) {
+          if (err) { done(err); return; }
+
+          assert.strictEqual(true, valid);
+
+          topic.setTitle('uhoh');
+
+          topic.validate(function(err, valid) {
+            if (err) { done(err); return; }
+
+            assert.strictEqual(false, valid);
+            assert.deepEqual(['is not included in the list'], topic.getErrors().get('title'));
+
+            done();
+          });
+        });
+      });
+    });
+
+    function tc(attrs, result, message) {
+      return function(cb) {
+        Topic.new(attrs, function(err, topic) {
+          if (err) { cb(err); return; }
+
+          topic.validate(function(err, valid) {
+            if (err) { cb(err); return; }
+
+            assert.strictEqual(result, valid);
+
+            cb();
+          });
+        });
+      };
+    }
+  });
+
+  test('validatesInclusionOf with allowNull', function(done) {
+    Topic.validatesInclusionOf('title', { in: 'a b c d e f g'.split(' '), allowNull: true });
+
+    async.parallel([
+      tc({ title: 'a!', content: 'abc' }, false),
+      tc({ title: '',   content: 'abc' }, false),
+      tc({ title: null, content: 'abc' }, true)
+    ], done);
+
+    function tc(attrs, result, message) {
+      return function(cb) {
+        Topic.new(attrs, function(err, topic) {
+          if (err) { cb(err); return; }
+
+          topic.validate(function(err, valid) {
+            if (err) { cb(err); return; }
+
+            assert.strictEqual(result, valid);
+
+            cb();
+          });
+        });
+      };
+    }
+  });
+
+  test('validatesInclusionOf with custom message', function(done) {
+    Topic.validatesInclusionOf('title', { in: 'a b c d e f g'.split(' '), message: 'option %(value)s is not in the list' });
+
+    Topic.new({ title: 'a', content: 'abc' }, function(err, topic) {
+      if (err) { done(err); return; }
+
+      topic.validate(function(err, valid) {
+        if (err) { done(err); return; }
+
+        assert(valid);
+
+        topic.setTitle('uhoh');
+
+        topic.validate(function(err, valid) {
+          if (err) { done(err); return; }
+
+          assert(!valid);
+          assert.deepEqual(['option uhoh is not in the list'], topic.getErrors().get('title'));
+
+          done();
+        });
+      });
+    });
+  });
+
+  test('validatesInclusionOf with within option', function(done) {
+    Topic.validatesInclusionOf('title', { within: 'a b c d e f g'.split(' ') });
+
+    Topic.new({ title: 'a', content: 'abc' }, function(err, topic) {
+      if (err) { done(err); return; }
+
+      topic.validate(function(err, valid) {
+        if (err) { done(err); return; }
+
+        assert(valid);
+
+        topic.setTitle('uhoh');
+
+        topic.validate(function(err, valid) {
+          if (err) { done(err); return; }
+
+          assert(!valid);
+          assert(topic.getErrors().get('title').length > 0);
+
+          done();
+        });
+      });
+    });
+  });
+
+  test('validatesInclusionOf with function', function(done) {
+    Topic.validatesInclusionOf('title', { within: function(topic) { return topic.getAuthorName() === 'sikachu' ? ['monkey', 'elephant'] : ['abe', 'wasabi']; } });
+
+    Topic.new(function(err, topic) {
+      if (err) { done(err); return; }
+
+      topic.setAuthorName('sikachu');
+      topic.setTitle('wasabi');
+
+      topic.validate(function(err, valid) {
+        if (err) { done(err); return; }
+
+        assert(!valid);
+
+        topic.setTitle('elephant');
+
+        topic.validate(function(err, valid) {
+          if (err) { done(err); return; }
+
+          assert(valid);
+
+          done();
+        });
+      });
+    });
+  });
+
+  test('validatesInclusionOf with invalid args', function() {
+    assert.throws(function() { Topic.validatesInclusionOf('title');                 });
+    assert.throws(function() { Topic.validatesInclusionOf('title', { in: null });   });
+    assert.throws(function() { Topic.validatesInclusionOf('title', { in: 0 });      });
+    assert.throws(function() { Topic.validatesInclusionOf('title', { in: 'abc' });  });
+  });
+});
